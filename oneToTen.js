9d5b8a407530782c9fbbd5a7fa21445aa923b626
7marie13 <124921535+7marie13@users.noreply.github.com>
2023-02-17 14:26:46 +0100

    Add files via upload

---
+++ oneToTen.js
@@ -0,0 +1,5 @@
+let som =0;
+for (let compteur = 1; compteur <= 10; compteur++ ) {
+som = som + compteur ;
+}
+console.log("sum of numbers 1 to 10:", som);
---
+++ weekendsVerified.js
@@ -0,0 +1,9 @@
+let day = "lundi";
+if (day != "samedi" || day != "dimanche" )
+{
+console.log("weekday");
+} 
+else{ 
+  console.log(weekend);
+}
+ 
